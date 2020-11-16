76c6f8187d4d2bcb2d74eb4053ae4cfa767eb31b
Lucio Pintanel <lm.pintanel@gmail.com>
2020-11-16 19:57:54 -0300

    - Desenvolvido o CRUD para tipo de medida.

--- app/main/controller/typeUnit_controller.py
+++ app/main/controller/typeUnit_controller.py
@@ -3,12 +3,16 @@ from flask_restx import Resource
 
 from ..util.dto import TUnitDto
 
-from ..service.type_unit_service import get_all_typeUnit, save_new_typeUnit
+from ..service.type_unit_service import get_all_typeUnit, save_new_typeUnit, update_typeUnit, del_type_unit
+from app.main.util.decorator import token_required
+
 
 api = TUnitDto.api
 _typeUnit = TUnitDto.type_unit
 
+
 @api.route('/')
+@api.route('/<id>')
 class TypeUnitList(Resource):
     @api.doc('list_of_registered_type_units')
     @api.marshal_list_with(_typeUnit, envelope='data')
@@ -17,9 +21,25 @@ class TypeUnitList(Resource):
         return get_all_typeUnit()
 
     @api.expect(_typeUnit, validate=True)
-    @api.response(201, 'User successfully created.')
+    @token_required
+    @api.response(201, 'Type Unit successfully created.')
     @api.doc('create a new type unit')
     def post(self):
         """Creates a new User """
         data = request.json
-        return save_new_typeUnit(data=data) 
+        return save_new_typeUnit(data=data)
+
+   
+    @api.expect(_typeUnit, validate=True)
+    @token_required
+    @api.response(201, 'Type Unit successfully updated.')
+    @api.doc('Update a type unit')
+    def put(self):
+        data = request.json
+        return update_typeUnit(data=data)
+
+    @token_required
+    @api.response(201, 'Type Unit successfully deleted.')
+    @api.doc('Delete a new type unit')
+    def delete(self,id):
+        return del_type_unit(__id=id)
--- app/main/service/type_unit_service.py
+++ app/main/service/type_unit_service.py
@@ -4,12 +4,12 @@ from app.main.model.typeunit import TypeUnit
 
 def save_new_typeUnit(data):
     typeUnit = TypeUnit.query.filter_by(typeunit=data['typeunit']).first()
-    if not typeUnit:        
+    if not typeUnit:
         new_typeUnit = TypeUnit(
             typeunit=data['typeunit']
         )
         __save_changes(new_typeUnit)
-        return { "mensagem" : "Cadastrado com sucesso no data base!" }
+        return {"mensagem": "Cadastrado com sucesso no data base!"}
     else:
         response_object = {
             'status': 'fail',
@@ -18,10 +18,48 @@ def save_new_typeUnit(data):
         return response_object, 409
 
 
+def update_typeUnit(data):
+    typeUnit = TypeUnit.query.filter_by(typeunit=data['typeunit']).first()
+    if typeUnit:
+        typeUnit.typeunit = data['typeunit_new']
+        __save_changes(typeUnit)
+        return {"mensagem": "Alterado com sucesso no data base!"}
+    else:
+        response_object = {
+            'status': 'fail',
+            'message': 'Type unit already exists. Please Log in.',
+        }
+        return response_object, 409
+
+
+def del_type_unit(__id):
+    typeUnit = TypeUnit.query.get(int(__id))
+    if typeUnit:
+        print("IF")
+        delete_changes(typeUnit)
+        response_object = {
+            'status': 'success',
+            'message': 'Successfully deleted.',
+            'public_id': __id
+        }
+        return response_object, 201
+    else:
+        response_object = {
+            'status': 'fail',
+            'message': 'Type Unit not exists. Please Log in.',
+        }
+        return response_object, 404
+
+
 def get_all_typeUnit():
     return TypeUnit.query.all()
 
 
 def __save_changes(data):
     db.session.add(data)
+    db.session.commit()
+
+
+def delete_changes(data):
+    db.session.delete(data)
     db.session.commit()
