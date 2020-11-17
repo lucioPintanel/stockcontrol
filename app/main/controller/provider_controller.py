214d62cb4b28992d90be82b8edbbfe83662e9279
Lucio Pintanel <lm.pintanel@gmail.com>
2020-11-16 21:54:48 -0300

    - Desenvolvido o processo de CRU, falta implementar o delete.

--- app/main/controller/provider_controller.py
+++ app/main/controller/provider_controller.py
@@ -3,23 +3,34 @@ from flask_restx import Resource
 
 from ..util.dto import ProviderDto
 
-from ..service.provider_service import get_all_provider, save_new_provider
+from ..service.provider_service import get_all_provider, save_new_provider, update_provider
+from app.main.util.decorator import token_required
 
 api = ProviderDto.api
 _provider = ProviderDto.provider
 
 @api.route('/')
 class ProviderList(Resource):
-    @api.doc('list_of_registered_type_units')
+    @api.doc('list_of_registered_provider')
     @api.marshal_list_with(_provider, envelope='data')
     def get(self):
         """ List all registered type units """
         return get_all_provider()
 
     @api.expect(_provider, validate=True)
-    @api.response(201, 'User successfully created.')
-    @api.doc('create a new type unit')
+    @token_required
+    @api.response(201, 'Provider successfully created.')
+    @api.doc('create a new provider')
     def post(self):
         """Creates a new User """
         data = request.json
         return save_new_provider(data=data) 
+
+    @api.expect(_provider, validate=True)
+    @token_required
+    @api.response(201, 'Provider successfully created.')
+    @api.doc('create a new provider')
+    def put(self):
+        """Update a provider """
+        data = request.json
+        return update_provider(data=data) 
--- app/main/service/provider_service.py
+++ app/main/service/provider_service.py
@@ -22,10 +22,58 @@ def save_new_provider(data):
         return response_object, 409
 
 
+def update_provider(data):
+    provider = Provider.query.filter_by(cnpj=data['cnpj']).first()
+    if provider:
+        if not provider.name == data['name']:
+            provider.name = data['name']
+
+        if not provider.contact == data['contact']:
+            provider.contact = data['contact']
+
+        if not provider.email == data['email']:
+            provider.email = data['email']
+
+        if not provider.telephone == data['telephone']:
+            provider.telephone = data['telephone']
+
+        db.session.commit()
+        return {"mensagem": "Alterado com sucesso no data base!"}
+    else:
+        response_object = {
+            'status': 'fail',
+            'message': 'Type unit already exists. Please Log in.',
+        }
+        return response_object, 409
+
+
+def del_provider(data):
+    provider = Provider.query.filter_by(cnpj=data['cnpj']).first()
+    if provider:
+        delete_changes(provider)
+        response_object = {
+            'status': 'success',
+            'message': 'Successfully deleted.',
+            'public_id': data['cnpj']
+        }
+        return response_object, 201
+    else:
+        response_object = {
+            'status': 'fail',
+            'message': 'Provider not exists. Please Log in.',
+        }
+        return response_object, 404
+
+
 def get_all_provider():
     return Provider.query.all()
 
 
 def __save_changes(data):
     db.session.add(data)
+    db.session.commit()
+
+
+def delete_changes(data):
+    db.session.delete(data)
     db.session.commit()
